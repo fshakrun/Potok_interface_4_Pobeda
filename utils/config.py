01649baadb1a75f47a57e6fb82e630c627b14a13
Fyodor Sh <82056292+fshakrun@users.noreply.github.com>
2026-03-23 22:15:02 +0300

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,2 +1,2 @@
-BASE_URL = "http://localhost:8000/app/flow.html"
+BASE_URL = "https://raw.githubusercontent.com/fshakrun/Potok_interface_4_Pobeda/main/app/Flow.html"
 API_BASE = "http://localhost:5000"
