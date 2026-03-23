ac997664587d3a95d9eb683b6031dc1cef390287
Fyodor Sh <82056292+fshakrun@users.noreply.github.com>
2026-03-23 21:58:22 +0300

    Update flow_page.py

--- pages/flow_page.py
+++ pages/flow_page.py
@@ -4,33 +4,45 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 class FlowPage:
-
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 10)
+        self.wait = WebDriverWait(driver, 15)
 
     def open(self, url):
         self.driver.get(url)
 
-    def click_first_edit(self):
+        # Ждём полной загрузки HTML
         self.wait.until(
-            lambda d: d.find_element("class name", "edit-btn")
-        ).click()
-        
+            EC.presence_of_element_located((By.TAG_NAME, "body"))
+        )
+
     def click_comments_tab(self):
-        tab = wait_for_element_clickable(self.driver, (By.ID, "comments-tab"))
+        tab = self.wait.until(
+            EC.element_to_be_clickable((By.ID, "comments-tab"))
+        )
+
+        # JS click стабильнее в CI
         self.driver.execute_script("arguments[0].click();", tab)
 
+        # ждём, что что-то появилось после клика
+        self.wait.until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div"))
+        )
+
     def open_search(self):
-        self.wait.until(EC.element_to_be_clickable((By.ID, "toggleSearch"))).click()
+        search_btn = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'Поиск')]"))
+        )
+        search_btn.click()
 
     def search(self, text):
-        input_el = self.wait.until(EC.visibility_of_element_located((By.ID, "searchInput")))
-        input_el.clear()
-        input_el.send_keys(text)
+        input_field = self.wait.until(
+            EC.presence_of_element_located((By.XPATH, "//input"))
+        )
+        input_field.clear()
+        input_field.send_keys(text)
 
     def get_items(self):
-        return self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "list-group-item")))
-
-    def toggle_bot(self):
-        self.wait.until(EC.element_to_be_clickable((By.ID, "botToggle"))).click()
+        return self.wait.until(
+            EC.presence_of_all_elements_located((By.XPATH, "//div"))
+        )
