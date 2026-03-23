d0feac55ba680016dbfe7bf9a74b861842c37fcf
Fyodor Sh <82056292+fshakrun@users.noreply.github.com>
2026-03-23 22:23:23 +0300

    Update test_search.py

--- tests/ui/test_search.py
+++ tests/ui/test_search.py
@@ -1,12 +1,11 @@
 import allure
-from pages.flow_page import FlowPage
+from selenium.webdriver.common.by import By
 from utils.config import BASE_URL
 
 
 @allure.feature("UI")
-@allure.story("Поиск")
 def test_search(driver):
-  driver.get(BASE_URL)
+    driver.get(BASE_URL)
 
     inputs = driver.find_elements(By.TAG_NAME, "input")
 
